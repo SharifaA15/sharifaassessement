17bd1fec6d405cd8b5f4c4bca71963d8b471a900
SharifaA15 <sharifa.abdirizak@gmail.com>
2021-11-23 06:11:38 -0600

    first commit

---
+++ functions.test.ts
@@ -0,0 +1,10 @@
+const {shuffleArray} = require('./utils')
+
+describe('shuffleArray should', () => {
+    // CODE HERE
+})
+ test('shuffleArray should return an array' , () => {
+ let arrCopy.length = arrCopy.length(shuffleArray)
+ expect(arrCopy).toBe('array')
+
+ })
